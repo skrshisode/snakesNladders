338b5abdb86bf7362b572741e7b8414775d133a8
Sushilkumar Shisode <shisoder@iitgn.ac.in>
2016-08-04 02:21:22 +0530

    added some game logic

--- README.md
+++ README.md
@@ -1,3 +1,9 @@
 #Snakes and ladders
 
-|Simple implementation of Snake and Ladder game in react
+Simple implementation of Snake and Ladder game in react
+
+Run:
+    1. `npm install`
+    2. `node bin/www`
+
+Then visit: [http://localhost:3000/](http://localhost:3000/)
--- public/js/app.js
+++ public/js/app.js
@@ -1,5 +1,7 @@
-/**
+/*
  * Issues: The only issues is that the player still survives even after snake bite :P
+ *
+ * j = 9 - parseInt((position-1)/10); i = (j%2==0)? 9-((position-1)-(9-j)*10):(position-1)-(9-j)*10;
  * 
  * Ideas: 
  * 1. Initially load some form elemtns to select the mode : single/multiplayer
@@ -7,9 +9,11 @@
  * 3. Notification bar to get some info
  * 4. 
  */
+'use strict';
 
 (function() {
 
+/*	
 	let Player = (function() {
 		'use strict';
 
@@ -20,15 +24,18 @@
 			}
 			// constructor body
 			this.name = name;
-			_position = 0;
+			var _position = 0;
+			this.setPosition = function(position){
+				_position = position;
+			};
+			this.getPosition = function() {
+				return _position;
+			};
 		}
-
-		// Player.prototype.throwDice = function() {
-		// 	return Math.round((Math.random() * 5)) + 1;
-		// };
 
 		return Player;
 	}());
+*/
 
 	let Snake = (function() {
 		'use strict';
@@ -39,7 +46,7 @@
 				return new Snake(head, tail);
 			}
 			// constructor body
-			if (head <= tail) throw Error("head should be at higher position than tail");
+			if (head <= tail) throw Error("Snake's head should be at higher position than tail");
 
 			this.head = head;
 			this.tail = tail;
@@ -69,7 +76,7 @@
 				return new Ladder(top, bottom);
 			}
 			// constructor body
-			if (top <= bottom) throw Error("top should be greater than bottom");
+			if (top <= bottom) throw Error("Ladder's top should be greater than bottom");
 
 			this.top = top;
 			this.bottom = bottom;
@@ -90,26 +97,26 @@
 		return Ladder;
 	}());
 
+
 	const Peg = React.createClass({
 		render: function() {
-			const pegColor = this.props.pegColor;
+			const types = ["peg__red","peg__green","peg__blue","peg__yellow"];
+			const pegNumber = this.props.pegNumber;
 			return(
-				<div className={!!pegColor? "peg "+pegColor:"peg"}></div>
+				<div className={!isNaN(pegNumber)? "peg "+types[pegNumber]:"peg"}></div>
 			);
 		},
 	});
 
+	// How to handle overlapping pegs in a Cell?
 	const Cell = React.createClass({
 		render: function() {
 			const col = this.props.colId;
 			const row = this.props.rowId;
 			const size = this.props.size;
+			const pegs = this.props.pegs;
 			const position = (row%2 == 0) ? (size-1-row)*10+(size-1-col)+1 : (size-1-row)*10+col+1;
-			return (
-				<div className="cell">
-					
-				</div>
-			);	
+			return pegs.includes(position) ? (<div className="cell"><Peg pegNumber={pegs.indexOf(position)}/></div>):(<div className="cell"></div>);	
 		},
 	});
 
@@ -117,7 +124,7 @@
 		render: function() {
 			var row = [];
 			for (var i = 0; i < this.props.size; i++) {
-				row.push(<Cell key={i} rowId={this.props.rowId} colId={i} size={this.props.size}/>);
+				row.push(<Cell key={i} rowId={this.props.rowId} colId={i} size={this.props.size} pegs={this.props.pegs}/>);
 			}
 			return (
 				<div className = "row">
@@ -131,7 +138,7 @@
 		render: function() {
 			var rows = [];
 			for (var i = 0; i < this.props.size; i++) {
-				rows.push(<Row key={i} rowId={i} size={this.props.size} />);
+				rows.push(<Row key={i} rowId={i} size={this.props.size} pegs={this.props.pegs} />);
 			}
 			return (
 				<div className="grid">
@@ -141,23 +148,110 @@
 		},
 	});
 
+	const Dice = React.createClass({
+		handleClick: function() {
+			this.props.throwDice();
+		},
+		render: function() {
+			return(
+				<div className="dice">
+					<p style={{textAlign:'center'}}>{this.props.value}</p>
+					<button type="button" style={{margin:'auto',width:'50px',display:'block'}} onClick={this.handleClick}>Throw</button>
+				</div>
+			);	
+		},
+	});
+
 	const GameBoard = React.createClass({
 		getInitialState: function() {
 			return {
+				status: 0, 	//0-Init,1-Started,2-Finished
+				notification: "Game is about to start",
 				num_of_players: 2,
 				multiplayer: false,
-				
+				dice: '?',
+				pegs: [0,0],
 			};
 		},
+		startGame: function(e) {
+			e.preventDefault();
+			let radios = document.getElementsByName('gameMode');
+			let flag = false;
+			let gameMode = "";
+			for (var i = radios.length - 1; i >= 0; i--) {
+				if(radios[i].checked){
+					flag = true;
+					gameMode = radios[i].value;
+				}
+			}
+
+			if (!flag) {
+				alert("Please select the mode!")
+			}else if(gameMode==="multiPlayer"){
+				this.setState({
+					status: 1,
+					notification: "Multiplayer game started",
+					num_of_players: 4,
+					multiplayer: true,
+					pegs: [0,0,0,0],
+				});
+			}else{
+				this.setState({
+					status: 1,
+					notification: "Single player game started",
+				});
+			}
+		},
+		updateDice: function() {
+			const number = Math.round((Math.random() * 5)) + 1;
+			this.setState({
+				notification: "Dice thrown",
+				dice: number,
+			});
+			this.updatePegPosition(number);
+		},
+		updatePegPosition: function(movement) {
+			let positions = this.state.pegs;
+			positions[0] =  positions[0]+movement;
+			this.setState({
+				pegs: positions
+			});
+			this.updateGame();
+		},
+		updateGame: function(){
+
+		},
 		render: function() {
+			if(this.state.status===0){
+				return (
+					<div className="intro">
+						<p style={{textAlign:'center'}}>
+							Select the mode: <br/>
+						</p>
+						<form onSubmit={this.startGame}>
+							<p style={{textAlign:'center'}}>
+								<input type="radio" name="gameMode" value="singlePlayer"/>Single Player (with CPU)<br/>
+								<input type="radio" name="gameMode" value="multiPlayer"/>Multiplayer<br/>
+								<input type="submit" style={{margin:'auto',marginTop:'25px',width:'100px',display:'block'}} value="Start Game"/>
+							</p>
+						</form>
+					</div>
+				);
+			}
+
 			return (
-				<p>Game</p>
+				<div className="gameboard">
+					<p style={{textAlign:'center'}}>{this.state.notification}</p>
+					<Grid size={10} pegs={this.state.pegs} />
+					<Dice value={this.state.dice} throwDice={this.updateDice}/>
+				</div>
 			);
 		},
 	});
 
-	ReactDOM.render( <Grid size={10} /> ,
+	ReactDOM.render( <GameBoard /> ,
 		document.getElementById('app')
 	);
 
 })();
+ 
